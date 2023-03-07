9f843a001c4f338926de80d2b3f557f5b0f11b82
Pasinee S <98906847+pasinee-sb@users.noreply.github.com>
2023-03-06 21:36:26 -0500

    attempted to make a test for random functions, but not successful

--- wordfinder.py
+++ wordfinder.py
@@ -4,7 +4,7 @@ import random
 class WordFinder:
     """Machine for finding random words from dictionary.
     
-    >>> wf = WordFinder("simple.txt")
+    >>> wf = WordFinder("words.txt")
     3 words read
 
     >>> wf.random() in ["cat", "dog", "porcupine"]
@@ -22,13 +22,12 @@ class WordFinder:
         """set path to link specified in function """
         self.path = path
         """open file and save to words"""
-        words = open(self.path, "r")
+        self.open = open(self.path, "r")
         """set words read in file in words prop"""
-        self.words = words.read()
+        self.words = self.open.read()
         """set all_words prop to list of string where each word is split by whitespaces """
         self.all_words = list(map(str,self.words.split()))
-        """close file after reading"""
-        words.close()
+        
         print (f"{len(self.words)} words read")
 
     def __repr__(self):
@@ -40,12 +39,18 @@ class WordFinder:
         return random.choice(self.all_words)
 
 class SpecialWordFinder(WordFinder):
+
+    """
+    >>> wf = SpecialWordFinder("special_word_test.txt")
+    >>> random.seed(2)
+    >>> wf.random()
+    
+    
+
+    """
     def __init__(self,path):
         super().__init__(path)
-    def random(self):
-        self.all_words = [word for word in self.words.split() if word[0]!= "#"]
-        return random.choice(self.all_words)
-
-
+        split_it = list(map(str,self.words.splitlines()))
+        self.all_words = [word for word in split_it if not word.startswith("#") and word != "" ]
     
     
